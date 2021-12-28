15f035f84b8297254d6adfcb62d1505d7c0fcac0
Cristian Romanescu <cristian@romanescu.ro>
2021-12-28 20:27:02 +0200

    Add --skip-config-check switch to automate deployments

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -3,9 +3,23 @@
 # Exit immediately with error
 set -e
 
+while test $# -gt 0; do
+        echo "$1"
+        case "$1" in
+                --skip-config-check)
+                    SKIP_CONFIG_CHECK="1"
+                    shift
+                    ;;
+                *)
+                    break
+                    ;;
+        esac
+done
+
 DEPLOY_TAG="$@"
 if [ ! "${DEPLOY_TAG}" ]; then
-        echo "Usage: ./deploy-prod.sh <git_tag_name>"
+        echo "Usage: ./deploy-prod.sh [--skip-config-check] <git_tag_name>"
+        echo "    --skip-config-check - Skip checking configuration changes"
         exit 1;
 fi
 
@@ -23,11 +37,14 @@ if [ ! "$(docker ps -a | grep $APP_CONTAINER_NAME)" ]; then
         exit 1;
 fi
 
-# Test for existing changes
-echo 'Testing for Drupal configuration changes ...'
-CONFIG_STATUS=$(docker exec "${APP_CONTAINER_NAME}" ./vendor/bin/drush config:status -n 2>&1)
-echo "${CONFIG_STATUS}" | grep -i -q -s "No differences" 
-echo "No changes, moving on ..."
+
+if [ -z "${SKIP_CONFIG_CHECK}" ]; then
+        # Test for existing Drupal configuration changes
+        echo 'Testing for Drupal configuration changes ...'
+        CONFIG_STATUS=$(docker exec "${APP_CONTAINER_NAME}" ./vendor/bin/drush config:status -n 2>&1)
+        echo "${CONFIG_STATUS}" | grep -i -q -s "No differences"
+        echo "No changes, moving on ..."
+fi
 
 APP_VERSION="${CONTAINER_APP_VERSION}"
 echo "Current app version: $APP_VERSION ..."
